0899bec0e3b245a16af9a2784edb91ea562830d1
Joseph Mingrone <jrm@ftfl.ca>
2018-01-11 21:10:19 -0400

    Update command to get StumpWM pid

--- ml_mem.sh
+++ ml_mem.sh
@@ -4,7 +4,7 @@ interval=3 # customize this
 mem_tot="$(awk '/real memory/ {print $4;exit}' /var/run/dmesg.boot)"
 mem_tot="$(( mem_tot/1024/1024 ))"
 #stump_pid="$(pgrep -a -n stumpwm)"
-stump_pid="$(pgrep -anf -U "$(id -u)" "sbcl .*/stumpwm/load.lisp")"
+stump_pid="$(pgrep -anf -U "$(id -u)" "sbcl .*(stumpwm:stumpwm)")"
 
 # while stumpwm is still running
 while kill -0 "$stump_pid" > /dev/null 2>&1; do
